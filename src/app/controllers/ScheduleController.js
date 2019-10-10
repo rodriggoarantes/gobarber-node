65f14f498b9f3da46a4e7514788fe89ef00b98eb
rodrigo <rodrigo.arantes@totvs.com.br>
2019-10-10 13:28:26 -0300

    Adicionado parametro para filtrar agendamentos por data

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,3 +1,5 @@
+import {startOfDay, endOfDay, parseISO} from 'date-fns';
+import { Op } from 'sequelize';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -6,8 +8,24 @@ class ScheduleController {
   async index(req, res) {
     const { page = 1, pageSize = 10 } = req.query;
 
+    const checkUserProvider = await User.findOne({where: {id: req.userId, provider: true}});
+    if (!checkUserProvider) {
+      return res.status(401).json({message: "Usuário não é um prestador de serviço"});
+    }
+
+    let dateCondition = {};
+    const {date} = req.query;
+    if (date) {
+      const parsedDate = parseISO(date);
+      dateCondition = {date: {
+        [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)]
+      }};
+    }
+
+    const filterCondition = { provider_id: req.userId, canceled_at: null };
+
     const appointments = await Appointment.findAll({
-      where: { provider_id: req.userId, canceled_at: null },
+      where: Object.assign(filterCondition, dateCondition),
       order: ['date'],
       limit: pageSize,
       offset: (page - 1) * pageSize,
