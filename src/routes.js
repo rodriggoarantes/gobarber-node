aa159a810a1b26d7064cd6b399ddb6c933eea623
rodrigo <rodrigo.arantes@totvs.com.br>
2019-10-11 14:23:17 -0300

    listagem de notificaçoes

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -29,5 +30,7 @@ routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 
 routes.get('/schedules', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
 
 export default routes;
