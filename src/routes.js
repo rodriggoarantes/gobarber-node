9b90c922bca1a54de51a0180cfdc744384977efa
rodrigo <rodrigo.arantes@totvs.com.br>
2019-11-26 11:40:46 -0300

    Criado endpoint para cadastro de usuario publico

--- src/routes.js
+++ src/routes.js
@@ -17,6 +17,7 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
+routes.post('/register', UserController.store);
 
 routes.use(authMiddleware);
 
